7b2e48b4a25a6b2f5ab76fed63d3f08d45a6fee5
kosukeya <189577816+kosukeya@users.noreply.github.com>
2026-08-23 09:55:26 +0900

    Stage 11A: add parametrized carrier experiment

---
+++ experiments/stage11a_parametrized.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from pprint import pprint
+
+from t_search.stage11_parametrized import stage11a_summary
+
+
+if __name__ == "__main__":
+    pprint(stage11a_summary())

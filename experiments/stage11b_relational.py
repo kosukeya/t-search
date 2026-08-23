936835f9b9fcd6db7cd94c8a0c0e6849935925f2
kosukeya <189577816+kosukeya@users.noreply.github.com>
2026-08-23 10:22:55 +0900

    Stage 11B: add relational experiment entrypoint

---
+++ experiments/stage11b_relational.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from pprint import pprint
+
+from t_search.stage11_relational import stage11b_summary
+
+
+if __name__ == "__main__":
+    pprint(stage11b_summary())

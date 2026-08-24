0c76868ad653ef21d35cdbc223584523418ddc4e
kosukeya <189577816+kosukeya@users.noreply.github.com>
2026-08-24 10:45:00 +0900

    Stage 13B: add path closure experiment entry point

---
+++ experiments/stage13b_paths.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from pprint import pprint
+
+from t_search.stage13_paths import stage13b_summary
+
+
+if __name__ == "__main__":
+    pprint(stage13b_summary())
